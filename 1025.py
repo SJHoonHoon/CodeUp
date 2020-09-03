f032cfae2f5c99b8ce11236398f4327dce5f0bc2
song <sjhoonhoon@gmail.com>
2020-09-03 10:39:09 +0900

    21~30

---
+++ 1025.py
@@ -0,0 +1,13 @@
+a=list(input())
+
+a[0] = int(a[0])*10000
+a[1] = int(a[1])*1000
+a[2] = int(a[2])*100
+a[3] = int(a[3])*10
+a[4] = int(a[4])*1
+
+print("[%d]" % a[0])
+print("[%d]" % a[1])
+print("[%d]" % a[2])
+print("[%d]" % a[3])
+print("[%d]" % a[4])
---
+++ 1027.py
@@ -0,0 +1,5 @@
+a, b, c = input().split(".")
+
+print(c, end='-')
+print(b, end='-')
+print(a)
